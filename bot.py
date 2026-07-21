e571e26f2f934f0af6dff180ece1d464ace27ca4
abdollahihamidm2000-arch <abdollahihamidm2000@gmail.com>
2026-07-22 02:56:45 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("Khadem Al Mahdi Bot")
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
+from database import *
+
+TOKEN = ""TOKEN = "8307409670:AAGDvUGaeBnvbsiZDDBXrxfuM4DLXFEIfyw"
+
+create_tables()
+
+app = ApplicationBuilder().token(TOKEN).build()
